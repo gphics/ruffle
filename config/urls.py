7bb397de7ea66b5fb465a5175cb28d1d95aab878
gphics2~ <abdulbasitabdulakeem234@gmail.com2~>
2024-06-26 21:25:56 +0100

    created urls.py file

--- config/urls.py
+++ config/urls.py
@@ -14,9 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("account/", include("account.urls")),
+    path("news/", include("news.urls")),
+    path("stream/", include("stream.urls")),
+    path("supports/", include("supports.urls")),
+    path("channel/", include("channel.urls")),
 ]
