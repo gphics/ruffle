18791c00f8759535839b2d06a18c758cafc0de63
gphics2~ <abdulbasitabdulakeem234@gmail.com2~>
2024-06-27 21:48:50 +0100

    database design

--- account/models.py
+++ account/models.py
@@ -1,3 +1,26 @@
 from django.db import models
-
+from django.conf import settings
+from shortuuid import uuid
+from supports.models import Timestamps
 # Create your models here.
+
+
+class Profile(Timestamps):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="profile"
+    )
+    avatar = models.JSONField(null = True)
+    location = models.CharField(max_length = 200)
+    public_id = models.CharField(default=uuid(), max_length = 50)
+    gender_choices = (("M", "Male"), ("F", "Female"))
+    gender = models.CharField(choices=gender_choices, default="M", max_length=1)
+    # following_channels = models.ManyToManyField("channel.Channel",related_name="fchannels")
+    # owned_channels = models.ManyToManyField("channel.Channel",related_name="ochannels")
+    # member_channels = models.ManyToManyField("channel.Channel",related_name="mchannels")
+
+    class Meta:
+        ordering = ["-created_at"]
+        # default_related_name = "profile"
+
+    def __str__(self):
+        return self.user.username
--- supports/models.py
+++ supports/models.py
@@ -1,3 +1,80 @@
 from django.db import models
-
+from django.conf import settings
+from shortuuid import uuid
 # Create your models here.
+
+
+class Timestamps(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    upated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Comment(Timestamps):
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name="comments",
+        null=True,
+        on_delete=models.CASCADE,
+    )
+    dep_comment = models.ForeignKey(
+        "self", related_name="deps", null=True, on_delete=models.CASCADE
+    )
+    content = models.TextField()
+    news = models.ForeignKey(
+        "news.News", related_name="comments", null=True, on_delete=models.CASCADE
+    )
+    stream = models.ForeignKey(
+        "stream.Stream",
+        related_name="comments",
+        null=True,
+        on_delete=models.CASCADE,
+    )
+    public_id = models.CharField(default=uuid(), max_length=50)
+
+    class Meta:
+        # default_related_name = "comments"
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return self.author.username
+
+
+class Tag(Timestamps):
+    title = models.CharField(max_length=100)
+
+    class Meta:
+        # default_related_name = "tags"
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return self.title
+
+
+class Report(Timestamps):
+    news = models.ForeignKey(
+        "news.News", null=True, on_delete=models.CASCADE, related_name="reported"
+    )
+    stream = models.ForeignKey(
+        "stream.Stream",
+        null=True,
+        on_delete=models.CASCADE,
+        related_name="reported",
+    )
+    public_id = models.CharField(default=uuid(), max_length=50)
+    content = models.TextField()
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name="reports",
+        null=True,
+        on_delete=models.CASCADE,
+    )
+
+    class Meta:
+        # default_related_name = "reports"
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return self.author.username
