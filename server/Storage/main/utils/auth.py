5cd202a4e39cc6742200e3a3a61374f501e7dc7d
gphics <abdulbasitabdulakeem234@gmail.com>
2024-09-07 07:10:58 +0100

    preparing for microservice interconnection

--- server/Storage/main/utils/auth.py
+++ server/Storage/main/utils/auth.py
@@ -2,8 +2,13 @@ import os
 import requests
 
 
-def validate_auth(token):
-    auth_url = os.getenv("AUTH_SERVER_URL")
-    req = requests.get(f"{auth_url}/validate-auth", headers={"Authorization": f"Token {token}"})
-    res = req.json()
-    return res
+def validate_auth(Token):
+    try:
+        auth_url = os.getenv("AUTH_SERVER_URL")
+        req = requests.get(
+            f"{auth_url}/validate-auth", headers={"Authorization": f"Token {Token}"}
+        )
+        res = req.json()
+        return res
+    except Exception as e:
+        return {"data": None, "err": {"msg":"something went wrong"}}
--- server/Storage/main/views.py
+++ server/Storage/main/views.py
@@ -6,8 +6,8 @@ from .utils.auth import validate_auth
 from .models import StorageManager as Storage
 from shortuuid import uuid
 from .utils.serializers import ReadStorageSerializer
-
-# Create your views here.
+from .utils.auth import validate_auth
+from .utils.genRes import generateResponse
 
 
 class Cloud(APIView):
@@ -22,14 +22,32 @@ class Cloud(APIView):
         """
         > This method requires the auth token header
         """
+        #####
+        # Authentication start
+        ####
+        Token = req.headers.get("Token", None)
+        if not Token:
+            return Response(
+                generateResponse(err={"msg": "Auth token must be provided"})
+            )
+        auth_res = validate_auth(Token)
+        auth_err = auth_res.get("err", None)
+        auth_err_detail = auth_res.get("detail", None)
+        if auth_err:
+            return Response(generateResponse(err={"msg": auth_err["msg"]}))
+        if auth_err_detail:
+            return Response(generateResponse(err={"msg": auth_err_detail}))
+        #####
+        # Authentication end
+        ####
         folder = req.GET.get("folder", None)
         many = req.GET.get("many", False)
         if not folder:
-            return Response({"data": None, "err": "folder must be provided"})
+            return Response(generateResponse(err={"msg": "folder must be provided"}))
 
         media = req.FILES.get("media", None)
         if not media:
-            return Response({"data": None, "err": "media must be provided"})
+            return Response(generateResponse(err={"msg": "media must be provided"}))
         try:
             cloud = CloudStorage(folder)
             if not many:
@@ -45,7 +63,7 @@ class Cloud(APIView):
                     public_id=public_id,
                     folder=folder,
                 )
-                return Response({"data": public_id, "err": None})
+                return Response(generateResponse({"msg": public_id}))
             else:
                 media_list = req.FILES.getlist("media")
                 result = cloud.multiple_upload(media_list)
@@ -63,44 +81,64 @@ class Cloud(APIView):
                         folder=folder,
                     )
                     output.append(public_id)
-                return Response({"data": output, "err": None})
+                return Response(generateResponse({"msg": output}))
 
         except Exception as e:
-            print(e)
-            return Response({"data": None, "err": "something went wrong"})
+            return Response(generateResponse(err={"msg": "something went wrong"}))
 
     def put(self, req):
         """
         > This method require the public_id of the object to be updated as a url query param
         """
+        #####
+        # Authentication start
+        ####
+        Token = req.headers.get("Token", None)
+        if not Token:
+            return Response(
+                generateResponse(err={"msg": "Auth token must be provided"})
+            )
+        auth_res = validate_auth(Token)
+        auth_err = auth_res.get("err", None)
+        auth_err_detail = auth_res.get("detail", None)
+        if auth_err:
+            return Response(generateResponse(err={"msg": auth_err["msg"]}))
+        if auth_err_detail:
+            return Response(generateResponse(err={"msg": auth_err_detail}))
+        #####
+        # Authentication end
+        ####
         media = req.FILES.get("media", None)
         public_id = req.GET.get("id", None)
         if not media:
-            return Response({"data": None, "err": "media must be provided"})
+            return Response(generateResponse(err={"msg": "media must be provided"}))
         if not public_id:
-            return Response({"data": None, "err": "public_id must be provided"})
+            return Response(generateResponse(err={"msg": "public_id must be provided"}))
         first = Storage.objects.filter(public_id=public_id)
         if not first.exists():
-            return Response({"data": None, "err": "media object does not exist"})
+            return Response(
+                generateResponse(err={"msg": "media object does not exist"})
+            )
         second = first[0]
         try:
             cloud = CloudStorage(second.folder)
             result = cloud.update(second.key, media)
             if not result:
-                return Response({"data": None, "err": "something went wrong"})
+                return Response(generateResponse(err={"msg": "something went wrong"}))
             second.url = result["url"]
             second.content_type = result["content_type"]
             second.key = result["key"]
             second.save()
-            return Response({"data": "media updated successfully", "err": None})
+            return Response(generateResponse({"msg": "media updated successfully"}))
 
         except Exception as e:
-            return Response({"data": None, "err": "something went wrong"})
+            return Response(generateResponse(err={"msg": "something went wrong"}))
 
     def get(self, req):
         """
         > This method requires the public_id as "id" url query param
         """
+
         public_id = req.GET.get("id", None)
         if not public_id:
             return Response({"data": None, "err": "public_id must be provided"})
@@ -114,19 +152,41 @@ class Cloud(APIView):
         """
         > This method requires the public_id as "id" url query param
         """
+        #####
+        # Authentication start
+        ####
+        Token = req.headers.get("Token", None)
+        if not Token:
+            return Response(
+                generateResponse(err={"msg": "Auth token must be provided"})
+            )
+        auth_res = validate_auth(Token)
+        auth_err = auth_res.get("err", None)
+        auth_err_detail = auth_res.get("detail", None)
+        if auth_err:
+            return Response(generateResponse(err={"msg": auth_err["msg"]}))
+        if auth_err_detail:
+            return Response(generateResponse(err={"msg": auth_err_detail}))
+        #####
+        # Authentication end
+        ####
         public_id = req.GET.get("id", None)
         if not public_id:
-            return Response({"data": None, "err": "public_id must be provided"})
+            return Response(generateResponse(err={"msg": "public_id must be provided"}))
         first = Storage.objects.filter(public_id=public_id)
         if not first.exists():
-            return Response({"data": None, "err": "media object does not exist"})
+            return Response(
+                generateResponse(err={"msg": "media object does not exists"})
+            )
         try:
             cloud = CloudStorage()
             second = cloud.delete(first[0].key)
             if not second:
-                return Response({"data": None, "err": "something went wrong"})
+                return Response(generateResponse(err={"msg": "something went wrong"}))
             first.delete()
-            return Response({"data": "media deleted", "err": None})
+            return Response(generateResponse({"msg": "media deleted"}))
 
         except Exception as e:
-            return Response({"data": None, "err": "media object does not exist"})
+            return Response(
+                generateResponse(err={"msg": "media object does not exists"})
+            )
